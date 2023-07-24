54240809297cb7dfe898d5ee07ba4a93dc5fc323
Jean Paulo Athanazio De Mei <93294014+Jean-Paulo-Public@users.noreply.github.com>
2023-07-24 04:23:35 +0000

    feat: testando o for no html

--- galeria/views.py
+++ galeria/views.py
@@ -3,7 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'galeria/index.html')
+    dados = {
+             1:{"title": "Python",
+                "text": "Python é uma linguagem de programação"},
+             2:{"title": "DJango",
+                "text": "Django e um framework python"}                
+            }
+    return render(request, 'galeria/index.html', {"cards": dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
