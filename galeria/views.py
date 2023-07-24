6df09abcc677c803f0045bba3f885016bdaf46f2
Jean Paulo Athanazio De Mei <93294014+Jean-Paulo-Public@users.noreply.github.com>
2023-07-24 05:03:35 +0000

    feat: puxando os dados do banco de dados

--- galeria/views.py
+++ galeria/views.py
@@ -1,17 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from galeria.models import Fotografia
 
 # Create your views here.
 def index(request):
-    dados = {
-             1:{"title": "Python",
-                "text": "Python é uma linguagem de programação",
-                "foto": "/assets/imagens/Python_Logo.webp"},
-             2:{"title": "DJango",
-                "text": "Django e um framework python",
-                "foto": "/assets/imagens/Django-Framework-Logo.png"}                
-            }
-    return render(request, 'galeria/index.html', {"cards": dados})
+    fotografia = Fotografia.objects.all()
+    return render(request, 'galeria/index.html', {"cards": fotografia})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
