c3ee3b70e19283d12a00d07ee8d19d9c2dede4bf
Jean Paulo Athanazio De Mei <93294014+Jean-Paulo-Public@users.noreply.github.com>
2023-07-24 04:36:43 +0000

    feat: criação do SQLite

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Fotografia(models.Model):
+    title = models.CharField(max_length=20, null=False, blank=False)
+    text = models.CharField(max_length=300, null=False, blank=False)
+    descricao = models.TextField(null=False, blank=False)
+    foto = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return f"Fotografia [nome={self.title}]"
